f92b87f888c7407f800ea3674b0fa0b2a71b81f7
Lunsk90 <alcides.lunsk90@gmail.com>
2019-11-05 10:05:50 -0600

    Login no cuanta

---
+++ app/src/main/java/com/example/crudmysqlsis22b/Acercade.java
@@ -0,0 +1,45 @@
+package com.example.crudmysqlsis22b;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+public class Acercade{
+
+    Dialog myDialog;
+    AlertDialog.Builder dialogo;
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acercade);
+    }*/
+
+    public void acercade(final Context context){
+        myDialog = new Dialog(context);
+        myDialog.setContentView(R.layout.activity_acercade);
+        myDialog.setTitle("acercade");
+        myDialog.setCancelable(true);
+
+       /* btn_buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                        String action;
+                        Intent intent = new Intent(context, MainActivity.class);
+                        context.startActivity(intent);
+
+                        myDialog.dismiss();
+
+            }
+        });
+*/
+       myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.DKGRAY));
+        myDialog.show();
+    }
+}
